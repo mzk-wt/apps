5eff7376a1f2a8bb7269df341cebaa352a29a694
mzk <uta.mizuki@gmail.com>
2021-08-06 21:30:20 +0900

    ver.0.1.0

--- docs/js/mixin/canvasUtils.js
+++ docs/js/mixin/canvasUtils.js
@@ -95,39 +95,140 @@ let canvasUtils = {
          * 画像を描画
          * @param {CanvasRenderingContext2D} ctx 2Dレンダリングコンテキスト
          * @param {*} src 画像のパス
-         * @param {*} x 
-         * @param {*} y 
-         * @param {*} w 
-         * @param {*} h 
+         * @param {*} dx 
+         * @param {*} dy 
+         * @param {*} dw 
+         * @param {*} dh 
          * @param {number} rotate 回転角
          */
-        drawImage: function(ctx, src, x, y, w, h, rotate = 0) {
+        drawImage: function(ctx, src, dx, dy, dw, dh, rotate = 0) {
             const img = new Image();
             img.src = src;
             img.onload = () => {
                 ctx.rotate(rotate);
-                ctx.drawImage(img, x, y, w, h);
+                ctx.drawImage(img, dx, dy, dw, dh);
                 ctx.rotate(-rotate);
             }
         },
 
         /**
-         * ファイルアップロードされた画像を描画
+         * 画像をトリミングして描画
+         * @param {CanvasRenderingContext2D} ctx 2Dレンダリングコンテキスト
+         * @param {*} src 画像のパス
+         * @param {*} x 
+         * @param {*} y 
+         * @param {*} w 
+         * @param {*} h 
+         * @param {*} dx 
+         * @param {*} dy 
+         * @param {*} dw 
+         * @param {*} dh 
+         * @param {number} rotate 回転角
+         */
+        drawImageTrim: function(ctx, src, x, y, w, h, dx, dy, dw, dh, rotate = 0) {
+            const img = new Image();
+            img.src = src;
+            img.onload = () => {
+                ctx.rotate(rotate);
+                ctx.drawImage(img, x, y, w, h, dx, dy, dw, dh);
+                ctx.rotate(-rotate);
+            }
+        },
+
+        /**
+         * アップロードされた画像を描画
+         * @param {CanvasRenderingContext2D} ctx 2Dレンダリングコンテキスト
+         * @param {*} data アップロードデータ 
+         * @param {*} dx 
+         * @param {*} dy 
+         * @param {*} dw 
+         * @param {*} dh 
+         * @param {number} rotate 回転角
+         */
+        drawUploadImage: function(ctx, data, dx, dy, dw, dh, rotate = 0) {
+            let reader = new FileReader();
+            reader.onload = () => {
+                let src = reader.result;
+                this.drawImage(ctx, src, dx, dy, dw, dh, rotate);
+            }
+            const blobData = this.cnvBlobData(data);
+            reader.readAsDataURL(blobData);
+        },
+
+        /**
+         * アップロードされた画像をトリミングして描画
          * @param {CanvasRenderingContext2D} ctx 2Dレンダリングコンテキスト
          * @param {*} data アップロードデータ 
          * @param {*} x 
          * @param {*} y 
          * @param {*} w 
          * @param {*} h 
+         * @param {*} dx 
+         * @param {*} dy 
+         * @param {*} dw 
+         * @param {*} dh 
          * @param {number} rotate 回転角
          */
-        drawUploadImage: function(ctx, data, x, y, w, h, rotate = 0) {
+        drawUploadImageTrim: function(ctx, data, x, y, w, h, dx, dy, dw, dh, rotate = 0) {
             let reader = new FileReader();
             reader.onload = () => {
                 let src = reader.result;
-                this.drawImage(ctx, src, x, y, w, h, rotate);
+                this.drawImageTrim(ctx, src, x, y, w, h, dx, dy, dw, dh, rotate);
             }
-            reader.readAsDataURL(data);
+            const blobData = this.cnvBlobData(data);
+            reader.readAsDataURL(blobData);
+        },
+
+        /**
+         * 画像データのサイズ（縦、横）を取得
+         * @param {*} data 画像データ 
+         * @param {*} callback 
+         */
+        getImageSize: function(data, callback) {
+            let reader = new FileReader();
+            reader.onload = () => {
+                const img = new Image();
+                img.src = reader.result;
+                img.onload = () => {
+                    callback(img.naturalWidth, img.naturalHeight);
+                }
+            }
+            const blobData = this.cnvBlobData(data);
+            reader.readAsDataURL(blobData);
+        },
+
+        /**
+         * base64形式のデータをBlob形式のデータに変換
+         * @param {*} base64 base64形式のデータ - "data:image/png;base64,iVBORw0k〜"
+         * @param {*} mime MIMEタイプ（デフォルト"image/png"）
+         * @returns Blob形式のデータ
+         */
+        cnvBlobData: function(base64, mime="image/png") {
+            const tmp = base64.split(",");
+            const data = atob(tmp[1]);
+
+            let buf = new Uint8Array(data.length);
+            for (let i = 0; i < data.length; i++) {
+                buf[i] = data.charCodeAt(i);
+            }
+
+            return new Blob([buf], {type: mime});
+        },
+
+        /**
+         * 
+         * @param {*} canvas 
+         * @param {*} w 
+         * @param {*} h 
+         * @returns 
+         */
+        resizeCanvas: function(canvas, w, h) {
+            const newCanvas = document.createElement("canvas");
+            newCanvas.width = w;
+            newCanvas.height = h;
+            const ctx = newCanvas.getContext('2d');
+            ctx.drawImage(canvas, 0, 0, w, h);
+            return newCanvas;
         }
     }
 }
